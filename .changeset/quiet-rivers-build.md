ccb7021647b6238e56aa1e932c1b4da1ba1b816a
ForeverSc <fengshuochn@126.com>
2026-06-06 17:31:11 +0800

    chore: migrate releases to semantic-release

--- .changeset/quiet-rivers-build.md
+++
@@ -1,5 +0,0 @@
----
-"@pinfix/plugin": patch
----
-
-Include the Chinese README in the published package and add Changesets release automation.
---
+++ release.config.mjs
@@ -0,0 +1,32 @@
+const packageRoot = "packages/unplugin";
+const packageName = "@pinfix/plugin";
+
+export default {
+  branches: ["main"],
+  tagFormat: `${packageName}@\${version}`,
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: `${packageRoot}/CHANGELOG.md`,
+        changelogTitle: `# ${packageName} Changelog`,
+      },
+    ],
+    [
+      "@semantic-release/npm",
+      {
+        pkgRoot: packageRoot,
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: [`${packageRoot}/package.json`, `${packageRoot}/CHANGELOG.md`],
+        message: `chore(release): ${packageName} \${nextRelease.version} [skip ci]\n\n\${nextRelease.notes}`,
+      },
+    ],
+    "@semantic-release/github",
+  ],
+};
